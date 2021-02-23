9c0e29710936ed477478f75d6cbeb6a579016b33
Vincent Yu <vantoriarin@gmail.com>
2021-02-23 18:24:49 +0900

    [add] Initialization of default files

---
+++ main.py
@@ -0,0 +1,56 @@
+import discord
+from discord.ext import commands
+import json
+
+# Get configuration.json
+with open("configuration.json", "r") as config: 
+	data = json.load(config)
+	token = data["token"]
+	prefix = data["prefix"]
+
+
+class Greetings(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
+		self._last_member = None
+
+# Intents
+intents = discord.Intents.default()
+
+bot = commands.Bot(prefix, intents = intents)
+
+# Load cogs
+initial_extensions = [
+	"Cogs.onCommandError",
+	"Cogs.help",
+	"Cogs.ping"
+]
+
+print(initial_extensions)
+
+if __name__ == '__main__':
+	for extension in initial_extensions:
+		try:
+			bot.load_extension(extension)
+		except Exception as e:
+			print(f"Failed to load extension {extension}")
+
+# @commands.Cog.listener()
+# async def on_member_join(self, member):
+@commands.Cog.listener()
+async def on_message(self, message):
+	if not(message.startswith("!mzk")):
+		return
+	message_cut = message[4:]
+	usr_cmd = message_cut.lower()
+
+	if usr_cmd == 'konnichiwa':
+		message.channel.send('Doumo')
+
+@bot.event
+async def on_ready():
+	print(f"We have logged in as {bot.user}")
+	await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name =f"{bot.command_prefix}help"))
+	print(discord.__version__)
+
+bot.run(token)
